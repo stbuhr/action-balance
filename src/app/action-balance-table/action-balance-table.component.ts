f115a0f3242d2ea3f31b7676299fbe3aea54aef5
Steffen Buhr <steffen.buhr@isb-ik.de>
2024-04-19 15:57:41 +0200

    Mostly ready

--- src/app/action-balance-table/action-balance-table.component.ts
+++ src/app/action-balance-table/action-balance-table.component.ts
@@ -1,6 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+} from '@angular/core';
 import { TranslocoModule } from '@jsverse/transloco';
+
+export type Meaning = 'personal' | 'activity' | 'methodical' | 'social';
+export type BalanceType = 'ideal' | 'expectation' | 'execution' | 'result';
+
+export interface BalanceColumn {
+  meaning: Meaning;
+  ideal: number;
+  expectation: number;
+  execution: number;
+  result: number;
+}
+
+interface BalanceLine {
+  meaning: string;
+  line: string;
+  strength: string;
+}
 
 @Component({
   selector: 'kode-action-balance-table',
@@ -10,4 +32,62 @@ import { TranslocoModule } from '@jsverse/transloco';
   styleUrl: './action-balance-table.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ActionBalanceTableComponent {}
+export class ActionBalanceTableComponent {
+  values = input<BalanceColumn[]>([]);
+
+  isValueHigh(value: number): boolean {
+    return value >= 10;
+  }
+  isValueMedium(value: number): boolean {
+    return value >= 6 && value < 10;
+  }
+
+  lines = computed<BalanceLine[]>(() => {
+    const lines: BalanceLine[] = [];
+
+    for (const col of this.values()) {
+      lines.push({
+        meaning: col.meaning,
+        line: 'hi-he',
+        strength: this.calcDifference(col.ideal, col.expectation),
+      });
+      lines.push({
+        meaning: col.meaning,
+        line: 'hi-hv',
+        strength: this.calcDifference(col.ideal, col.execution),
+      });
+      lines.push({
+        meaning: col.meaning,
+        line: 'hi-hr',
+        strength: this.calcDifference(col.ideal, col.result),
+      });
+      lines.push({
+        meaning: col.meaning,
+        line: 'he-hv',
+        strength: this.calcDifference(col.expectation, col.execution),
+      });
+      lines.push({
+        meaning: col.meaning,
+        line: 'he-hr',
+        strength: this.calcDifference(col.expectation, col.result),
+      });
+      lines.push({
+        meaning: col.meaning,
+        line: 'hv-hr',
+        strength: this.calcDifference(col.execution, col.result),
+      });
+    }
+
+    return lines;
+  });
+
+  calcDifference(first: number, second: number): string {
+    if (Math.abs(first - second) > 3) {
+      return 'high';
+    }
+    if (Math.abs(first - second) == 3) {
+      return 'low';
+    }
+    return 'none';
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ActionBalanceTableComponent } from './action-balance-table/action-balance-table.component';
+import {
+  ActionBalanceTableComponent,
+  BalanceColumn,
+} from './action-balance-table/action-balance-table.component';
 
 @Component({
   selector: 'kode-root',
@@ -11,4 +14,16 @@ import { ActionBalanceTableComponent } from './action-balance-table/action-balan
 })
 export class AppComponent {
   title = 'action-balance';
+  balance = signal<BalanceColumn[]>([
+    { meaning: 'personal', ideal: 7, expectation: 6, execution: 5, result: 7 },
+    { meaning: 'activity', ideal: 8, expectation: 6, execution: 7, result: 10 },
+    {
+      meaning: 'methodical',
+      ideal: 9,
+      expectation: 11,
+      execution: 10,
+      result: 8,
+    },
+    { meaning: 'social', ideal: 6, expectation: 7, execution: 8, result: 5 },
+  ]);
 }
