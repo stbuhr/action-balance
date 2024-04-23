826b8568d3116a818067dbf4e3972acd3819a728
Steffen Buhr <steffen.buhr@isb-ik.de>
2024-04-23 08:18:01 +0200

    Config transloco

--- transloco.config.js
+++ transloco.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  rootTranslationsPath: 'src/assets/i18n/',
-  langs: ['de', 'en'],
-  keysManager: {}
+  rootTranslationsPath: "/action-balance/assets/i18n/",
+  langs: ["de", "en"],
+  keysManager: {},
 };
